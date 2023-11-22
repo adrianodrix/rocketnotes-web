bd09a903ee570e76561d898da2f8e17651b42374
Adriano Santos <adrianodrix@gmail.com>
2023-11-22 01:14:11 -0300

    fix: logout redirect to home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,10 +3,17 @@ import { Container, Profile, Logout } from "./styles"
 import { useAuth } from '../../hooks/auth'
 import { api } from '../../services/api'
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom'
 
 export function Header() {
     const { signOut, user } = useAuth()
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+    const navigate = useNavigate()
+
+    function handleSignOut() {
+        navigate('/')
+        signOut()
+    }
 
     return (
         <Container>
@@ -18,7 +25,7 @@ export function Header() {
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
